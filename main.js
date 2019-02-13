bb8a5998dfba5e97b8bbb96fa4efcb912d39e5c3
Adriana <adrirx5@gmail.com>
2019-02-13 14:05:32 +0100

    splash hecho

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 'use strict'
+
+const main = ()=> {
+  const buildDom = (html)=> {
+    const main = document.querySelector('main');
+    main.innerHTML = html;
+    return main;
+  };
+
+  const buildSplashScreen = ()=> {
+    const splashScreen = buildDom(`
+      <h1>Hello</h1>
+      <section class="splash-screen">
+        <h1>Eternal Enemies</h1>
+        <button>Start</button>
+      </section>
+    `);
+
+  };
+
+  buildSplashScreen();
+
+}
+
+window.addEventListener('load', main);
+console.log('hey');
